d90f3e72fe3676b20c86d2bce4d6f3a549a47958
vagrant <vagrant@devbox.vaimo.test>
2019-11-12 16:26:39 +0100

    iu form button edit

---
+++ view/frontend/web/js/view/element/buttonSubmit.js
@@ -0,0 +1,10 @@
+define([
+    'Magento_Ui/js/form/components/button'
+    ], function (Element) {
+    'use strict';
+    return Element.extend({
+        action: function () {
+
+        },
+    })
+})
